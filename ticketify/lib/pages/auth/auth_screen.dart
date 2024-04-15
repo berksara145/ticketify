38d092ab2d65367119fb07785e91bd3724a4ce9f
unknown <at1llaes22@gmail.com>
2024-04-15 14:43:31 +0300

    added loginForm

--- ticketify/lib/pages/auth/auth_screen.dart
+++ ticketify/lib/pages/auth/auth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ticketify/constants/constant_variables.dart';
+import 'package:ticketify/pages/auth/widgets/login_form.dart';
 import 'package:ticketify/pages/auth/widgets/register_form.dart';
 
 class AuthScreen extends StatelessWidget {
@@ -16,32 +17,36 @@ class AuthScreen extends StatelessWidget {
     return Scaffold(
       body: Row(
         children: <Widget>[
-          Expanded(
-            child: Container(
-              color: AppColors.green,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset(
+          Container(
+            color: AppColors.green,
+            width: screenWidth * 11 / 18,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(
+                    left: 12,
+                  ),
+                  child: Image.asset(
                     AssetLocations.compwoman,
                     width: screenWidth / 2,
                     height: screenHeight / 2,
                   ),
-                  Text(
-                    'TICKETIFY',
-                    style: GoogleFonts.allertaStencil(
-                      textStyle: TextStyle(
-                        color: AppColors.black,
-                        fontSize: 70,
-                        fontWeight: FontWeight.bold,
-                      ),
+                ),
+                Text(
+                  'TICKETIFY',
+                  style: GoogleFonts.allertaStencil(
+                    textStyle: TextStyle(
+                      color: AppColors.black,
+                      fontSize: 70,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          Expanded(child: RegisterForm())
+          Expanded(child: LoginForm())
         ],
       ),
     );
