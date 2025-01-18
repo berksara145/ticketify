1903bd81d76cd06b1494e79e15fc266179628366
berk <91435139+berksara145@users.noreply.github.com>
2025-01-18 23:35:18 +0300

    a
    a

--- ticketify/lib/config/api_config.dart
+++ ticketify/lib/config/api_config.dart
@@ -1,10 +1,6 @@
 class ApiConfig {
   static String get baseUrl {
-    final uri = Uri.base;
-    if (uri.host == '127.0.0.1' || uri.host == 'localhost') {
-      return 'http://127.0.0.1:5000';
-    } else {
-      return 'http://13.60.25.33';
-    }
+    final uri = "http://13.60.25.33";
+    return uri;
   }
 }
