9205bfc98d3559347bc970d0c8963d9b24c62887
berk <91435139+berksara145@users.noreply.github.com>
2025-01-19 10:55:19 +0300

    a
    a

--- ticketify/build/web/flutter_service_worker.js
+++ ticketify/build/web/flutter_service_worker.js
@@ -36,14 +36,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "460a50d213074469d79a8ea3376887a9
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "d0be969df9d09ee3991390d2b23a15ca",
+"flutter_bootstrap.js": "9b02233befda09324c75cdc4774bfa8b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e28c271032a0e997d4d3feecd8888e6c",
-"/": "e28c271032a0e997d4d3feecd8888e6c",
-"main.dart.js": "997b3537ab55a2b7d9135bb31a76837f",
+"index.html": "75c97cf16c41fe8568aab5f5bf408048",
+"/": "75c97cf16c41fe8568aab5f5bf408048",
+"main.dart.js": "f3dad5235605a635af4d33ed15f2abe0",
 "manifest.json": "7fd72f50a3a3323614819d1d72aca559",
 "version.json": "df31762775fdff492a91dd085a5bb7bd"};
 // The application shell files that are downloaded before a service worker can
